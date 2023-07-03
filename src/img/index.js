3604cd9dc37283cccedef3a195ea99ed4f654fb1
Viviane Qian <viviane@diverta.co.jp>
2023-07-03 12:43:12 +0900

    annotation style adjustments

--- src/img/index.js
+++ src/img/index.js
@@ -20,16 +20,15 @@ router.post('/generate-annotations', async (req) => {
 
 	const htmlAnnotations = translatedAnnotations.reduce((acc, annotation) => {
 		const { x, y } = annotation.vertices[0];
-		const width = annotation.vertices[2].x - x;
-		const height = annotation.vertices[2].y - y;
-		const style = `position: absolute; left: ${x}px; top: ${y}px; width: ${width}px; height: ${height}px; display: flex; align-items: center; justify-content: center; font-size: 12px; font-family: sans-serif; font-weight: bold; color: black; background: white;`;
+		const fontSize = annotation.vertices[2].y - y;
+		const style = `position: absolute; left: ${x}px; top: ${y}px; font-size: ${fontSize}px; background: white; padding: 0.3em;`;
 		return `${acc}<div style="${style}">${annotation.translated}</div>`;
 	}, '');
 
 	// send result as single key string object for KurocoEdge to be able to capture it as a string
 	return new Response(
 		JSON.stringify({
-			htmlAnnotations,
+			htmlAnnotations: htmlAnnotations,
 		}),
 		{
 			headers: {
