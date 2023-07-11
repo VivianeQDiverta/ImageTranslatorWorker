0deea0d66591c71131d03de93333bcb73febcc50
Viviane Qian <viviane@diverta.co.jp>
2023-07-11 15:41:46 +0900

    compute annotation style when not sent

--- src/img/index.js
+++ src/img/index.js
@@ -2,6 +2,28 @@ import { Router } from 'itty-router';
 
 export const router = Router({ base: '/img' });
 
+const computeAnnotationStyle = async (annotation) => {
+	const translated = annotation.translated.replace(/\n/g, '<br>');
+	if (annotation.x && annotation.y && annotation.fontSize) {
+		return {
+			x: annotation.x,
+			y: annotation.y,
+			fontSize: annotation.fontSize,
+			translated,
+		};
+	}
+	const numberOfLines = annotation.translated.match(/\n/g)?.length + 1 || 1;
+	const { x, y } = annotation.vertices[0];
+	const height = annotation.vertices[2].y - y;
+	const fontSize = numberOfLines > 1 ? (height / numberOfLines) * 0.8 : height;
+	return {
+		x,
+		y,
+		fontSize,
+		translated,
+	};
+};
+
 router.post('/generate-annotations', async (req) => {
 	const body = await req.text();
 	const { translatedAnnotations } = body ? JSON.parse(decodeURIComponent(body)) : {};
@@ -20,13 +42,9 @@ router.post('/generate-annotations', async (req) => {
 	}
 
 	const htmlAnnotations = translatedAnnotations.reduce((acc, annotation) => {
-		const numberOfLines = annotation.translated.match(/\n/g)?.length + 1 || 1;
-		annotation.translated = annotation.translated.replace(/\n/g, '<br>');
-		const { x, y } = annotation.vertices[0];
-		const height = annotation.vertices[2].y - y;
-		const fontSize = numberOfLines > 1 ? (height / numberOfLines) * 0.8 : height;
+		const { x, y, fontSize, translated } = computeAnnotationStyle(annotation);
 		const style = `text-align: start; position: absolute; left: ${x}px; top: ${y}px; background: white; font-size: ${fontSize}px; width: max-content;`;
-		return `${acc}<div style="${style}">${annotation.translated}</div>`;
+		return `${acc}<div style="${style}">${translated}</div>`;
 	}, '');
 
 	const wrapppedHtmlAnnotations = `<div class="annotationsContainer">${htmlAnnotations}</div>`;
